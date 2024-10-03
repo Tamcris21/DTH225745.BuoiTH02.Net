cb25fd6f39e2541c854ca225481982d2c5a01bdc
TamCris <tamcris502@gmail.com>
2024-10-03 09:07:47 +0700

    BaiTH02

---
+++ Buoi 02/Bai2.2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai2._2
+{
+    public partial class frmBai2 : Form
+    {
+        public frmBai2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Buoi 02/Bai2.3/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai2._3
+{
+    public partial class frmBai2_3 : Form
+    {
+        public frmBai2_3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHienThi_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Chao mung ban den voi mon hoc LAP TRINH .NET");
+        }
+    }
+}
